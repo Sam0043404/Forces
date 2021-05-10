9deefd6afaf550f1c5085434f66d35ee3bed14eb
Samuel Henaghan <RC0043404@student.riversidecollege.ac.uk>
2021-05-10 14:51:25 +0100

    added use for button

--- Forces/Form1.Designer.cs
+++ Forces/Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -107,11 +109,31 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "°";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 446);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fx = ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Fy = ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(720, 468);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
@@ -122,7 +144,7 @@
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
-            this.Text = "│";
+            this.Text = "Forces";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -139,6 +161,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Forces/Form1.cs
+++ Forces/Form1.cs
@@ -18,28 +18,26 @@ namespace Forces
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-            double sin(double x)
-            {
-                return (Math.Sin(x * Math.PI / 180.0));
-            }
-            double asin(double x)
-            {
-                return (Math.Asin(x) * 180 / Math.PI);
-            }
-            double cos(double x)
-            {
-                return (Math.Cos(x * Math.PI / 180.0));
-            }
-            double acos(double x)
-            {
-                return (Math.Acos(x) * 180 / Math.PI);
-            }
+        { 
             
-            
-
         }
 
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI/180.0));
+        }
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180/Math.PI);
+        }
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI/180.0));
+        }
+        double acos(double x)
+        {
+            return (Math.Acos(x) * 180/Math.PI);
+        }
         private void PictureBox1_Click(object sender, EventArgs e)
         {
 
@@ -52,7 +50,14 @@ namespace Forces
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            double Force = double.Parse(textBox1.Text);
+            double Angle = double.Parse(textBox2.Text);
 
+            double Fx = Force * cos(Angle);
+            double Fy = Angle * sin(Angle);
+
+            label5.Text = "Fy = " + Fy;
+            label4.Text = "Fx = " + Fx;
         }
     }
 }
